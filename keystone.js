6cdccdfb6407be41ead394bf9ec6e82a7a2fd21b
vamshi9 <vamsisai999@gmail.com>
2017-10-21 14:54:28 +0530

    schedule initial setup & restricting access

--- keystone.js
+++ keystone.js
@@ -68,20 +68,21 @@ keystone.set('nav', {
 	users: 'users',
 });
 
+//requireUser
+keystone.set('signin url','/');
 
-
-// Start Keystone to connect to your database and initialise the web server
-
-
-if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
-	console.log('----------------------------------------'
-	+ '\nWARNING: MISSING MAILGUN CREDENTIALS'
-	+ '\n----------------------------------------'
-	+ '\nYou have opted into email sending but have not provided'
-	+ '\nmailgun credentials. Attempts to send will fail.'
-	+ '\n\nCreate a mailgun account and add the credentials to the .env file to'
-	+ '\nset up your mailgun integration');
-}
+// // Start Keystone to connect to your database and initialise the web server
+//
+//
+// if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
+// 	console.log('----------------------------------------'
+// 	+ '\nWARNING: MISSING MAILGUN CREDENTIALS'
+// 	+ '\n----------------------------------------'
+// 	+ '\nYou have opted into email sending but have not provided'
+// 	+ '\nmailgun credentials. Attempts to send will fail.'
+// 	+ '\n\nCreate a mailgun account and add the credentials to the .env file to'
+// 	+ '\nset up your mailgun integration');
+// }
 
 
 keystone.start();
--- routes/index.js
+++ routes/index.js
@@ -34,11 +34,12 @@ var routes = {
 // Setup Route Bindings
 exports = module.exports = function (app) {
 	// Views
-	app.get('/', routes.views.index);
+	app.get('/',middleware.requireUser,routes.views.index);
 	app.get('/blog/:category?', routes.views.blog);
 	app.get('/blog/post/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);
 	app.all('/contact', routes.views.contact);
+	app.get('/schedule',routes.views.schedule);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);

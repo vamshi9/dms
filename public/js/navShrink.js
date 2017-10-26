a98934e9a65274f0c0a7f344abf91cf2819b55d1
vamshi9 <vamsisai999@gmail.com>
2017-10-26 20:43:48 +0530

    migration from bootstrap3.7 to 4.0 beta

---
+++ public/js/navShrink.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+     $(window).scroll(function(){
+          if($(document).scrollTop()>50){
+                $('nav').addClass('shrink');
+          }else{
+                $('nav').removeClass('shrink');
+          }
+     });
+});

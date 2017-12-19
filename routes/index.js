29d9096ed1a13c71c17819fe311b6faf96a8d43b
vamshi9 <vamsisai999@gmail.com>
2017-12-19 16:11:30 +0530

    authentication

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ var routes = {
 
 exports = module.exports = function (app) {
 	// Views
-	app.get('/',routes.views.index);
+	app.get('/',middleware.requireUser,routes.views.index);
 	app.get('/blog/:category?', routes.views.blog);
 	app.get('/blog/post/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);

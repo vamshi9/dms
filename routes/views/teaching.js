2c19f9a51b5926e0531595ceee824675d524927e
vamshi9 <vamsisai999@gmail.com>
2018-05-03 19:23:08 +0530

    teaching header

--- routes/views/teaching.js
+++ routes/views/teaching.js
@@ -7,26 +7,27 @@ exports = module.exports = function(req,res){
     var locals = res.locals;
     locals.section = 'teaching';
     locals.data = {
-        teachingData :[]
+        teachingData :[],
+        year : ''
     }
     view.on('init',function(next){
         var year = req.body.year || 2018;
         var teaching = Teaching.model.find().where('year',year).populate('author');
         teaching.exec(function(err,results){
             locals.data.teachingData = results;
-        //     for(var x in locals.data.teachingData){
-        //         var updatedby = locals.data.teachingData[x].updatedBy;
-        //         var userInfo = User.model.findOne(updatedby).exec(function(err,user){
-        //            //locals.data.teachingData[x].updatedBy = "vamshi"; 
-        //           console.log(x + " is  " + locals.data.teachingData[x]);
-        //          // locals.data.teachingData[x].concat({'a' : 'b'});
-        //         });
-        //        // locals.data.teachingData[x] = JSON.stringify(locals.data.teachingData[x]) + {'a' : 'b'};
-        //     }
-        //    console.log(locals.data.teachingData)            
+            locals.data.year = year;
+            //for(var x in locals.data.teachingData){
+              //  locals.data.teachingData[x].year = year;
+                //var userInfo = User.model.findOne(updatedby).exec(function(err,user){
+                   //locals.data.teachingData[x].updatedBy = "vamshi"; 
+                  //console.log(x + " is  " + locals.data.teachingData[x]);
+                 // locals.data.teachingData[x].concat({'a' : 'b'});
+               // });
+               // locals.data.teachingData[x] = JSON.stringify(locals.data.teachingData[x]) + {'a' : 'b'};
+            //}
+           //console.log(locals.data.teachingData)            
             next(err);
         });    
     });
-
     view.render('teaching');     
 }

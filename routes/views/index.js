434fdf43c45f269f1c5c2c7ec740bfb11cf61d71
vamshi9 <vamsisai999@gmail.com>
2018-10-15 17:02:22 +0530

    todo list

--- routes/views/index.js
+++ routes/views/index.js
@@ -3,8 +3,8 @@ const Professors = keystone.list('professors');
 
 exports = module.exports = function (req, res) {
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
+	const view = new keystone.View(req, res);
+	const locals = res.locals;
 
 	locals.section = 'home';
 	locals.data = {
@@ -13,7 +13,7 @@ exports = module.exports = function (req, res) {
 
 	// Load the Publications projects
 	view.on('init', function (next) {
-		var professors = Professors.model.find().sort('ranking');
+		const professors = Professors.model.find().sort('ranking');
 
 		professors.exec(function (err, results) {
 			locals.data.professorsList = results;
@@ -22,5 +22,10 @@ exports = module.exports = function (req, res) {
 		});
 
 	});
+
+	/**
+	// todo: user tour through website
+	*/
+
 	view.render('index');
 };
